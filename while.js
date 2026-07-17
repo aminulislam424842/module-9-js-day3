043b82a6c9159ff765c35118953f4a2dc0761fe7
Aminul islam <aminulislam424842@gmail.com>
2026-07-17 22:11:22 +0600

    docs: add JavaScript while loop fundamentals

---
+++ while.js
@@ -0,0 +1,17 @@
+/*
+While Loop:
+
+- jotokkhon condition TRUE thakbe totokkhon loop cholbe
+- condition FALSE hole loop bondho
+
+Syntax:
+
+while(condition){
+    // code
+}
+
+Flow:
+1. condition check kore
+2. TRUE hole loop run kore
+3. abar condition check kore
+*/
