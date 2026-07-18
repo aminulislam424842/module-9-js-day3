5fc1915abacced7bd723cb880d5ff1f9f42e2bc5
Aminul islam <aminulislam424842@gmail.com>
2026-07-18 18:12:56 +0600

    docs: add break statement notes and syntax

---
+++ break/break.js
@@ -0,0 +1,15 @@
+/*
+Break Statement:
+
+- loop er moddhe use hoy (for, while, do-while)
+- loop ke sathe sathe bondho kore
+- ar kono iteration execute hoy na
+
+mane:
+- condition match korlei loop theke ber hoye jai
+- loop er porer code execute hoy
+
+Syntax:
+
+break;
+*/
