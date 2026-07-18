ad061fd73d66a42bf538d892218549073d1f9f2e
Aminul islam <aminulislam424842@gmail.com>
2026-07-18 18:40:34 +0600

    docs: add continue statement notes and syntax

---
+++ continue/continue.js
@@ -0,0 +1,16 @@
+/*
+Continue Statement:
+
+- loop er moddhe use hoy (for, while, do-while)
+- current iteration skip kore
+- next iteration e chole jai
+
+mane:
+- loop bondho kore na (break er moto na)
+- sudhu oi step ta skip kore
+
+Syntax:
+
+continue;
+*/
+
