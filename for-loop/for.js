cafe0f32b8630a2df26b2c4f1ad49d823a2366e6
Aminul islam <aminulislam424842@gmail.com>
2026-07-18 19:27:22 +0600

    docs: add for loop notes and syntax

---
+++ for-loop/for.js
@@ -0,0 +1,19 @@
+/*
+For Loop:
+
+- ek line e initialization, condition, increment lekha hoy
+- mainly count controlled loop e use hoy
+
+Syntax:
+
+for(initialization; condition; increment/decrement){
+    // code
+}
+
+Flow:
+1. initialization (ekbar run hoy)
+2. condition check
+3. TRUE hole code run
+4. increment/decrement
+5. abar condition check
+*/
